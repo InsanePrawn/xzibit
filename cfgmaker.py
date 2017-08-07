21cd89ab12e7a9a4a2a59e1de72879542b720f61
InsanePrawn <insane.prawny@gmail.com>
2017-08-07 02:57:44 +0200

    whoops

--- cfgmaker.py
+++ cfgmaker.py
@@ -39,7 +39,6 @@ def join_paths(base, suffix):
 
 def files_in_folder(prefix, base_dir, path_currently='', depth=0, max_depth=250):
     results = {}
-    print(base_dir, path_currently)
     with os.scandir(os.path.join(base_dir, path_currently)) as dir:
         log('working on ' + path_currently, True)
         for entry in dir:
